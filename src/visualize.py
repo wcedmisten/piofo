b22d133d6547ced4afc8ab3792f653bc4dbf2036
William Edmisten <wcedmisten@gmail.com>
2022-06-20 19:43:09 -0400

    Augment OCR Json with GPS coordinates

--- src/visualize.py
+++ src/visualize.py
@@ -5,7 +5,10 @@ import json
 import gpxpy
 import gpxpy.gpx
 
-GPX_FILE = '/home/wedmisten/piofo/20220531181027_004969/20220531181027_004969.gpx'
+FILE_NAME = '20220605145527_005152'
+WORKING_DIR = f'/home/wedmisten/piofo/{FILE_NAME}'
+
+GPX_FILE = f'{WORKING_DIR}/{FILE_NAME}.gpx'
 
 # get a list of coordinates from the GPX file
 gpx_points = []
@@ -19,9 +22,26 @@ with open(GPX_FILE, 'r') as gpx_file:
 
 # read the OCR data
 ocr_data = {}
-with open("/home/wedmisten/piofo/data.json", 'r') as ocr_data_file:
+with open(f"{WORKING_DIR}/ocr.json", 'r') as ocr_data_file:
     ocr_data = json.load(ocr_data_file)
 
+for key in ocr_data.keys():
+    tmp = ocr_data[key]
+    ocr_data[key] = {}
+    ocr_data[key]['ocr'] = tmp
+
+# Add, markers to the gps-locations we read from the images
+for idx, latlon in enumerate(gpx_points):
+    filepath = f'{WORKING_DIR}/{FILE_NAME}_{(idx+1):03}.jpg'
+
+    if filepath not in ocr_data:
+        print(filepath)
+        ocr_data[filepath] = {'ocr': []}
+
+    ocr_data[filepath]['latlon'] = latlon
+
+    if 'speed' in ocr_data[filepath]['ocr'] or 'limit' in ocr_data[filepath]['ocr']:
+        print(ocr_data[filepath]['ocr'], ocr_data[filepath]['latlon'])
 
 folium_map = create_folium_map()
 for track in read_gpx_file(GPX_FILE):
@@ -31,9 +51,9 @@ for track in read_gpx_file(GPX_FILE):
 
 # Add, markers to the gps-locations we read from the images
 for idx, latlon in enumerate(gpx_points):
-    filepath = f'/home/wedmisten/piofo/20220531181027_004969/20220531181027_004969_{(idx+1):03}.jpg'
+    filepath = f'{WORKING_DIR}/{FILE_NAME}_{(idx+1):03}.jpg'
 
-    words = "<br>".join(ocr_data.get(filepath, []))
+    words = "<br>".join(ocr_data.get(filepath, {}).get('ocr', []))
 
     marker = folium.Marker(
         location=latlon,
@@ -42,8 +62,12 @@ for idx, latlon in enumerate(gpx_points):
         show=(idx % 10 == 0),
     ))
     marker.add_to(folium_map)
+
 boundary = folium_map.get_bounds()
 folium_map.fit_bounds(boundary, padding=(3, 3))
 
 # Display initial map:
-folium_map.save('test.html')
+folium_map.save(f'{WORKING_DIR}/test.html')
+
+with open(f"{WORKING_DIR}/ocr_gpx.json", 'w') as f:
+    json.dump(ocr_data, f, indent=2)
