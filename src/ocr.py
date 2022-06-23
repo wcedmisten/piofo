9b759e30c4a02cf08952c2da2fcfe7a99eb1fd39
William Edmisten <wcedmisten@gmail.com>
2022-06-22 22:38:59 -0400

    Add checkpoint logic for running pipeline

--- src/ocr.py
+++ src/ocr.py
@@ -1,36 +1,41 @@
 import json
 import glob
 
+import os
+
 import keras_ocr
 
 # OUTPATH="/home/wedmisten/piofo/20220605145527_005152"
 
 
 def run_ocr(OUTPATH, BATCH_SIZE=4):
-    pipeline = keras_ocr.pipeline.Pipeline(scale=1)
+    if os.path.exists(f"{OUTPATH}/ocr.json"):
+        return
+    else:
+        pipeline = keras_ocr.pipeline.Pipeline(scale=1)
 
-    images = glob.glob(f"{OUTPATH}/*.jpg")
+        images = glob.glob(f"{OUTPATH}/*.jpg")
 
-    print("Running OCR on ", len(images), "images")
+        print("Running OCR on ", len(images), "images")
 
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]
+        def chunks(lst, n):
+            """Yield successive n-sized chunks from lst."""
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
 
-    data = {}
+        data = {}
 
-    for group in chunks(images, BATCH_SIZE):
-        images = [
-            keras_ocr.tools.read(url) for url in group
-        ]
+        for group in chunks(images, BATCH_SIZE):
+            images = [
+                keras_ocr.tools.read(url) for url in group
+            ]
 
-        # Each list of predictions in prediction_groups is a list of
-        # (word, box) tuples.
-        prediction_groups = pipeline.recognize(images)
+            # Each list of predictions in prediction_groups is a list of
+            # (word, box) tuples.
+            prediction_groups = pipeline.recognize(images)
 
-        for filename, prediction in zip(group, prediction_groups):
-            data[filename] = list(map(lambda pred: pred[0], prediction))
+            for filename, prediction in zip(group, prediction_groups):
+                data[filename] = list(map(lambda pred: pred[0], prediction))
 
-    with open(f"{OUTPATH}/ocr.json", 'w') as f:
-        json.dump(data, f, indent=2)
+        with open(f"{OUTPATH}/ocr.json", 'w') as f:
+            json.dump(data, f, indent=2)
--- src/pipeline.py
+++ src/pipeline.py
@@ -7,38 +7,52 @@ import shutil
 import subprocess
 
 import os
+from pathlib import Path
+
+# create the GPX file and extract 1-second images
+def create_gpx_and_jpg(input_mp4, gpx_filename, VIDEO_WORKSPACE):
+    if not os.path.exists(input_mp4):
+        print("Video file not found! ", input_mp4)
+        return
+
+    if not os.path.exists(VIDEO_WORKSPACE):
+        print("Creating workspace ", VIDEO_WORKSPACE)
+        os.mkdir(VIDEO_WORKSPACE)
+
+    # print("Deleting directory: ", VIDEO_WORKSPACE)
+    # shutil.rmtree(f"{WORKSPACE_DIR}/{fname_base}")
+
+    if not os.path.exists(gpx_filename):
+        print("Creating GPX file")
+        # generate the GPX file
+        main(['-i', f'{input_mp4}', '-o', gpx_filename, '-f'])
+    
+    if not os.path.exists(f"{VIDEO_WORKSPACE}/{fname_base}_001.jpg"):
+        print("Extracting images")
+        # extract 1-second images
+        ffmpeg_cmd = ['ffmpeg', '-i', input_mp4, '-qscale:v', '1', '-vf', "crop=2560:1395:0:0", '-r', '1', f"{VIDEO_WORKSPACE}/{fname_base}_%03d.jpg"]
+
+        rc = subprocess.call(ffmpeg_cmd)
+
 
 # get the path of the "piofo" directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 WORKSPACE_DIR = f'{BASE_DIR}/workspace'
 
-fname_base = '20220531181027_004969'
-input_mp4='/home/wedmisten/Videos/dashcam_2/20220531181027_004969.MP4'
+input_mp4='/home/wedmisten/Videos/dashcam_2/20220602164844_005000.MP4'
+fname_base = Path(input_mp4).stem
+
 
 VIDEO_WORKSPACE = f"{WORKSPACE_DIR}/{fname_base}"
 
 gpx_filename = f'{VIDEO_WORKSPACE}/{fname_base}.gpx'
 
-# create the GPX file and extract 1-second images
-def create_gpx_and_jpg():
-    print("Deleting directory: ", VIDEO_WORKSPACE)
-    # shutil.rmtree(f"{WORKSPACE_DIR}/{fname_base}")
-
-    os.mkdir(VIDEO_WORKSPACE)
-
-    # generate the GPX file
-    main(['-i', f'{input_mp4}', '-o', gpx_filename, '-f'])
-    # extract 1-second images
-    ffmpeg_cmd = ['ffmpeg', '-i', input_mp4, '-qscale:v', '1', '-vf', "crop=2560:1395:0:0", '-r', '1', f"{VIDEO_WORKSPACE}/{fname_base}_%03d.jpg"]
-
-    rc = subprocess.call(ffmpeg_cmd)
-
 # step 1, create the GPX and JPG files
-# create_gpx_and_jpg()
+create_gpx_and_jpg(input_mp4, gpx_filename, VIDEO_WORKSPACE)
 
 # step 2, run OCR on the images
-# ocr.run_ocr(VIDEO_WORKSPACE)
+ocr.run_ocr(VIDEO_WORKSPACE)
 
 # step 3, search the OSM DB for nearest roads
 osm_search.search_osm(fname_base, VIDEO_WORKSPACE, gpx_filename)
